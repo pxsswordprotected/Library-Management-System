e47cab173e05c492b77d2e84dc306fdf5ffeb42e
James <jamesalford661@gmail.com>
2024-05-01 22:09:24 -0500

    staffhandler start

--- src/objecthandlers/StaffHandler.java
+++ src/objecthandlers/StaffHandler.java
@@ -1,5 +1,20 @@
 package objecthandlers;
 
+import userobjects.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import utils.SQLUtil;
+
 public class StaffHandler {
+    private SQLUtil sqlUtil;
+
+    public StaffHandler() {
+        sqlUtil = new SQLUtil();
+    }
+    
 
 }
