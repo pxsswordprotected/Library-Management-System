22f04e39faf8ce961cc7b6fc39acf8dd7b1fc3e1
Boiled-Swift <grant.a.swift@gmail.com>
2024-05-02 23:25:17 -0500

    as

--- src/objecthandlers/StudentHandler.java
+++ src/objecthandlers/StudentHandler.java
@@ -52,6 +52,19 @@ public class StudentHandler {
         return students;
     }
 
+    public boolean login(String userName, String password) {
+        String query = "SELECT * FROM Student WHERE userName = '" + userName + "' AND password = '" + password + "'";
+        try {
+            ResultSet resultSet = sqlUtil.executeQuery(query);
+            if (resultSet.next()) {
+                return true;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(StudentHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false; 
+    }
+
     public SQLUtil getSqlUtil() {
         return this.sqlUtil;
     }
